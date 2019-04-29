b55c5bf4bcadddd17522d177479b65cd135ccac5
KamilBienias <kamil.bienias.math@gmail.com>
2019-04-29 08:27:13 +0200

    unmarshaller converts an xml file to objects

--- UnmarshallerExample.java
+++ UnmarshallerExample.java
@@ -5,6 +5,7 @@ import javax.xml.bind.Unmarshaller;
 
 public class UnmarshallerExample {
 
+    //converts an xml file to objects
     public static Unmarshaller generateUnmarshaller() throws JAXBException {
         JAXBContext ctx = JAXBContext.newInstance(Database.class);
         Unmarshaller unmarshaller = ctx.createUnmarshaller();
